bf274f101a0d3b490681a6e1d9bab087b6ae23ab
Anis Dlakic <anis.dlakic@amplitudo.me>
2022-03-09 11:37:11 +0100

    Added homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,19 @@
+// import "./homepage.styles.scss";
+
+const Homepage = () => {
+	return (
+		<div className="homepage">
+			<h1>Welcome to my Homepage</h1>
+			<div className="directory-menu">
+				<div className="menu-item">
+					<div className="content">
+						<div className="title">HATS</div>
+						<span className="subtitle">SHOP NOW</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Homepage;
